c98a3f17fb871bb7cd3dfd5976cbb63dd07fef8f
Maксим Шестов <maxim.very.cool2000@gmail.com>
2025-02-16 19:18:40 +0200

    firstCommit
    finaly

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelector(".hero-arrows").addEventListener("click", function () {
+  document.querySelector("main").scrollIntoView({ behavior: "smooth" });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollToTopBtn = document.getElementById("scrollToTop");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      scrollToTopBtn.classList.add("show");
+    } else {
+      scrollToTopBtn.classList.remove("show");
+    }
+  });
+
+  scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+});
